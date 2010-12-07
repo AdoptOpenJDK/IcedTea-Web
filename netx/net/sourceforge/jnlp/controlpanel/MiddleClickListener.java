1a6123c332f07e578d4086575c6ae3956723de59
Andrew Su <asu@redhat.com>
2010-12-07 14:50:36 -0500

    Added copyright header to MiddleClickListener.java. Corrected a typo in javadoc.

--- netx/net/sourceforge/jnlp/controlpanel/MiddleClickListener.java
+++ netx/net/sourceforge/jnlp/controlpanel/MiddleClickListener.java
@@ -1,3 +1,20 @@
+/* MiddleClickListener.java -- Update configuration when pasting with middle click.
+Copyright (C) 2010 Red Hat
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
 package net.sourceforge.jnlp.controlpanel;
 
 import java.awt.event.MouseAdapter;
@@ -9,7 +26,7 @@ import javax.swing.JTextField;
 import net.sourceforge.jnlp.runtime.DeploymentConfiguration;
 
 /**
- * When middle click pastes to the checkboxes it doesn't register it... This is
+ * When middle click pastes to the textboxes it doesn't register it... This is
  * to fix that problem. Not needed in Windows.
  * 
  * @author Andrew Su <asu@redhat.com, andrew.su@utoronto.ca>
