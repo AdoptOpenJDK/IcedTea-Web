fc7a9ca40e49c45b1152267e99179daff512544e
Andrew Azores <aazores@redhat.com>
2014-01-29 11:34:24 -0500

    Fix regression in previous PR1513 patch
    Fix for regression due to PR1513 fix. ClassLoader was too optimistic about
    finding codebase main-classes and so the not-all-signed dialog would
    appear even for applets that were entirely broken and could not be loaded
    at all.
    * netx/net/sourceforge/jnlp/Launcher.java: (createApplet,
    createAppletObject) pass enableCodeBase to JNLPClassLoader
    * netx/net/sourceforge/jnlp/runtime/JNLPClassLoader.java: (enableCodeBase)
    removed, now done by constructor argument. (checkNotAllSignedWithUser)
    minor refactor. (initializeResources) actually check if main-class is
    loadable from codebase when this is suspected, rather than assuming it
    will be there

--- netx/net/sourceforge/jnlp/Launcher.java
+++ netx/net/sourceforge/jnlp/Launcher.java
@@ -694,13 +694,7 @@ public class Launcher {
     protected  AppletInstance createApplet(JNLPFile file, boolean enableCodeBase, Container cont) throws LaunchException {
          AppletInstance appletInstance = null;
          try {
-            JNLPClassLoader loader = JNLPClassLoader.getInstance(file, updatePolicy);
-
-            if (enableCodeBase) {
-                loader.enableCodeBase();
-            } else if (file.getResources().getJARs().length == 0) {
-                throw new ClassNotFoundException("Can't do a codebase look up and there are no jars. Failing sooner rather than later");
-            }
+            JNLPClassLoader loader = JNLPClassLoader.getInstance(file, updatePolicy, enableCodeBase);
 
             ThreadGroup group = Thread.currentThread().getThreadGroup();
 
@@ -741,13 +735,7 @@ public class Launcher {
      */
     protected Applet createAppletObject(JNLPFile file, boolean enableCodeBase, Container cont) throws LaunchException {
         try {
-            JNLPClassLoader loader = JNLPClassLoader.getInstance(file, updatePolicy);
-
-            if (enableCodeBase) {
-                loader.enableCodeBase();
-            } else if (file.getResources().getJARs().length == 0) {
-                throw new ClassNotFoundException("Can't do a codebase look up and there are no jars. Failing sooner rather than later");
-            }
+            JNLPClassLoader loader = JNLPClassLoader.getInstance(file, updatePolicy, enableCodeBase);
 
             String appletName = file.getApplet().getMainClass();
             Class<?> appletClass = loader.loadClass(appletName);
