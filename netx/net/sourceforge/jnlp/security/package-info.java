99bd03be5b9ad4484d730d0088157a9e2e15926a
Jiri Vanek <jvanek@redhat.com>
2015-06-15 12:44:41 +0200

    Updated NEWS, added javadoc fo security package. Added changelog entry for last commit * NEWS: mentioned major work in last month and half * netx/net/sourceforge/jnlp/security/package-info.java: new file, describing flow of SecurityDialogMessage * ChangeLog: added content to previous item

---
+++ netx/net/sourceforge/jnlp/security/package-info.java
@@ -0,0 +1,86 @@
+/* package-info.java
+   Copyright (C) 2015 Red Hat, Inc.
+
+This file is part of IcedTea.
+
+IcedTea is free software; you can redistribute it and/or modify it under the
+terms of the GNU General Public License as published by the Free Software
+Foundation, version 2.
+
+IcedTea is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+IcedTea; see the file COPYING. If not, write to the
+Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+02110-1301 USA.
+
+Linking this library statically or dynamically with other modules is making a
+combined work based on this library. Thus, the terms and conditions of the GNU
+General Public License cover the whole combination.
+
+As a special exception, the copyright holders of this library give you
+permission to link this library with independent modules to produce an
+executable, regardless of the license terms of these independent modules, and
+to copy and distribute the resulting executable under terms of your choice,
+provided that you also meet, for each linked independent module, the terms and
+conditions of the license of that module. An independent module is a module
+which is not derived from or based on this library. If you modify this library,
+you may extend this exception to your version of the library, but you are not
+obligated to do so. If you do not wish to do so, delete this exception
+statement from your version.*/
+/**
+ *<pre>
+        ITW-thread(s)               | dialogs-thread                                                                                                                                  X
+                                    |                                                                                                                                                 X
+   presteps in SecurtyDialogs       |                                                                                                                                                 X
+      # eg handle trustall/none     |                                                                                                                                                 X
+            |                       |                                                                                                                                                 X
+    prepare message                 |                                                                                                                                                 X
+               # set JNLPfile       |                                                                                                                                                 X
+               # set type of dialog |                                                                                                                                                 X
+                   see              |                                                                                                                                                 X
+               # extrass...         |                                                                                                                                                 X
+               # lock               |                                                                                                                                                 X
+            |                       |                                                                                                                                                 X
+    post message to queue           |                                                                                                                                                 X
+            | >------------------------------------> | <----------------------------------------------------------------------------------------------------------------------------| X
+    wait for result from            |            read message from queue                                                                                                            | X
+          getUserResponse(lock lock)|                |                                                                                                                              | X
+                                    |            create instance of dialogue                                                                                                        | X
+                                    |                |                                                                                                                              | X
+                                    |            acording to type of dialogue, create and place panel  (this is important, panel is keeper of rememberable decission)               | X
+                                    |                |                                                                                                                              | X
+                                    |            if panel is instance of RememberableDialogue                                                                                       | X
+                                    |              else                        then                                                                                                 | X
+                                    |                |                          |                                                                                                   | X
+                                    |                |                         check whether this applet+action was already stored and pernamently remembered in .appletSecurity    | X
+                                    |                |                          no                                                                        yes                       | X
+                                    |                |<-------------------------|                                                                          |                        | X
+                                    |                |                                                                                                     |                        | X
+                                    |                |                                                                                                     |                        | X
+                                    |                |                                                                                                     |                        | X
+                                    |                |                                                                                                     |                        | X
+                                    |            add closing and disposing listener(s) to button(s)                                                        |                        | X
+                                    |                |     * set return value to listner?                                                                  |                        | X
+                                    |                |                                                                                                     |                        | X
+                                    |                      accordingo to set value, set default selected button?                                           |                        | X
+                                    |                |                                                                                                     |                        | X
+                                    |            if his applet+action was already stored in .appletSecurity include text approved/denyed and when          |                        | X
+                                    |                |                                                                                                     |                        | X
+                                    |            wait for user to click buttton or close dialogue                                                          |                        | X
+                                    |                |                                                                                                     |                        | X
+                                    |                --> set selected value (via listener?) to message, dispose dialog -> <- set stored value to message <-|                        | X
+                                    |                                                                                    |                                                          | X
+                                    |                    if panel is instance of RememberableDialogue crate new, update old(date/decission,jars...) record in .appletSecurity       | X
+                                    |                                                                                    |                                                          | X
+            | <------------------------------------------------------------------------------------------< unlock lock of this messge  >--------------------------------------------| X
+  read result from message          |                                                                                                                                                 X
+            |                       |                                                                                                                                                 X
+   continue accordingly             |                                                                                                                                                 X
+                                                                                                                                                                                      X
+
+ *</pre>
+ */
+package net.sourceforge.jnlp.security;
