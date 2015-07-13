8f654f0a7e6e8f1d3821fd43e5b50d407e9ff995
Jiri Vanek <jvanek@redhat.com>
2015-07-13 17:19:00 +0200

    Added tests for unsigned applet/app using Permission attribute

--- tests/reproducers/cloneReproducer.sh
+++ tests/reproducers/cloneReproducer.sh
@@ -9,12 +9,15 @@ to=$2
 familyFrom=$3
 familyTo=$4
 #^simple,signed,signed2,custom (script will proabbly not work fine for custom)
-#run from this dir, or use absolute path
-top=.
-#top=/home/jvanek/hg/icedtea-web/tests/reproducers
+#run from this dir (tests/reproducers), or use absolute path
+top1=.
+#top1=/home/jvanek/hg/icedtea-web/tests/reproducers
+top2=$top1
+#top2=/home/jvanek/Desktop
 
-srcDir=$top/$familyFrom
-destDir=$top/$familyTo
+
+srcDir=$top1/$familyFrom
+destDir=$top2/$familyTo
 
 desc=$destDir/$to
 src=$srcDir/$from
@@ -30,12 +33,19 @@ done
 tree  $desc
 cp -vr $src/srcs/META-INF  $desc/srcs/
 SED1="sed s/$from/$to/g"
+SED2="sed s/a/a/"
+if [ \( $familyFrom = signed -o $familyFrom = signed2 \) -a $familyTo = simple ] ; then
+SED2='sed s/bgcolor=\"red\"/bgcolor=\"blue\"/g'
+fi;
+if [ \( $familyTo = signed -o $familyTo = signed2 \) -a $familyFrom = simple ] ; then
+SED2='sed s/bgcolor=\"blue\"/bgcolor=\"red\"/g'
+fi;
 
 for x in $drs ; do
 	files=`ls $src/$x/`
 	for f in $files ; do 
 		target=`echo $f | $SED1`
-		cat $src/$x/$f | $SED1  | sed "s/@Test/@Test FIXME!/g" >  $desc/$x/$target
+		cat $src/$x/$f | $SED1  | sed "s/@Test/@Test FIXME!/g" | $SED2 >  $desc/$x/$target
 	done
 done
 
