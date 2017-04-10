13301671404ba508cb36e1b4fa4d40959ed2d19d
Jiri Vanek <jvanek@redhat.com>
2017-04-10 13:10:17 +0200

    On windows, shutdown hook added like we do, via lazy initiated OutputController (which may be swing/awt hidden call (not verified)) causes exception "can't add shutdown hook, shutdown already in progress" * netx/net/sourceforge/jnlp/util/docprovider/TextsProvider.java: (main) enforced headless mode, to prevent itw debug console from showing.

--- netx/net/sourceforge/jnlp/util/docprovider/TextsProvider.java
+++ netx/net/sourceforge/jnlp/util/docprovider/TextsProvider.java
@@ -54,6 +54,7 @@ import net.sourceforge.jnlp.config.Defaults;
 import net.sourceforge.jnlp.OptionsDefinitions;
 import net.sourceforge.jnlp.config.InfrastructureFileDescriptor;
 import net.sourceforge.jnlp.config.Setting;
+import net.sourceforge.jnlp.runtime.JNLPRuntime;
 import net.sourceforge.jnlp.runtime.Translator;
 import net.sourceforge.jnlp.util.docprovider.formatters.formatters.Formatter;
 import net.sourceforge.jnlp.util.docprovider.formatters.formatters.HtmlFormatter;
@@ -396,10 +397,13 @@ public abstract class TextsProvider {
     }
 
     public static void main(String[] args) throws IOException {
+        // Shutdown hook from OutputController was causing hanging build on Windows. It's not used on headless.
+        JNLPRuntime.setHeadless(true);
+
         if (args.length == 0) {
             System.out.println(" * IcedTea-Web self documentation tool list of arguments *");
-            System.out.println(" * arument version - last parameter of each command, is used when there is no internal versionknown *");
-            System.out.println(" *                 - is mandatory, but not used if real version do exists *");
+            System.out.println(" * argument version - last parameter of each command, is used when there is no internal versionknown *");
+            System.out.println(" *                  - is mandatory, but not used if real version do exists *");
             System.out.println(" * argument expand - one before last argument, false/true - is used to not/expand variables *");
             System.out.println(" * -------------------- *");
             System.out.println("all expand version - will generate all reports in theirs defaults into current directory");
