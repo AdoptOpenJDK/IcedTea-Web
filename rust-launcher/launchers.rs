a120a9bf571d3fa33a216b90958135a4d7336724
Jiri Vanek <jvanek@redhat.com>
2017-12-12 15:09:45 +0100

    built-time variables propaagted to rust lunchers * Makefile.am: preprocessing hardoced_paths.rs.in for each binary and creating libs * rust-launcher/launchers.rs: now uses new, future lib * rust-launcher/hardoced_paths.rs.in: source for all hardcoded libs we currently use

--- rust-launcher/launchers.rs
+++ rust-launcher/launchers.rs
@@ -1,3 +1,9 @@
+/*extern crate hardcoded_paths;*/
+extern crate hardcoded_paths;
+
 fn main() {
-    println!("Hello, world!");
+    println!("{}",hardcoded_paths::PROGRAM_NAME);
+    println!("{}",hardcoded_paths::MAIN_CLASS);
+    println!("{}",hardcoded_paths::JAVA);
+    println!("{}",hardcoded_paths::JRE);
 }
