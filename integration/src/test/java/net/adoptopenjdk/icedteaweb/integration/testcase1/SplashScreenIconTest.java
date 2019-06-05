9e5fbe1c1529b44e9467dded583b9a2f5faaf589
AndreasEhret <andreas.ehret@karakun.com>
2019-06-05 07:40:30 +0200

    add jnlp with icon element to information element, add test

---
+++ integration/src/test/java/net/adoptopenjdk/icedteaweb/integration/testcase1/SplashScreenIconTest.java
@@ -0,0 +1,41 @@
+package net.adoptopenjdk.icedteaweb.integration.testcase1;
+
+import com.github.tomakehurst.wiremock.junit.WireMockRule;
+import net.adoptopenjdk.icedteaweb.integration.IntegrationTest;
+import net.adoptopenjdk.icedteaweb.integration.TemporaryItwHome;
+import net.adoptopenjdk.icedteaweb.integration.testcase1.applications.SimpleJavaApplication;
+import net.sourceforge.jnlp.runtime.Boot;
+import org.junit.Rule;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+/**
+ * TODO: Scenario Description
+ */
+public class SplashScreenIconTest implements IntegrationTest {
+    private static final String JAR_NAME = "App-SimpleJavaApplication.jar";
+
+    @Rule
+    public TemporaryItwHome tmpItwHome = new TemporaryItwHome();
+    @Rule
+    public WireMockRule wireMock = new WireMockRule(wireMockConfig().dynamicPort());
+
+    @Test(timeout = 5_000)
+    public void testSplashIcon() throws IOException {
+        // given
+        final String jnlpUrl = setupServer(wireMock, "SimpleJavaApplicationWithSplash.jnlp", SimpleJavaApplication.class, JAR_NAME);
+        tmpItwHome.createTrustSettings(jnlpUrl);
+
+        // when
+        final String[] args = {"-jnlp", jnlpUrl, "-nosecurity", "-Xnofork", "-headless"};
+        Boot.main(args);
+
+        // then
+        assertThat(hasCachedFile(tmpItwHome, "javaws.png"), is(true));
+    }
+}
