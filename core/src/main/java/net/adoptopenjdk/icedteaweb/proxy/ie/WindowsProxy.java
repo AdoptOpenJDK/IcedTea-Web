59290e18697288de135089134c5b5ac6478222ef
Hendrik Ebbers <hendrik.ebbers@web.de>
2019-11-26 13:15:54 +0100

    test for all needed parameters

--- core/src/main/java/net/adoptopenjdk/icedteaweb/proxy/ie/WindowsProxy.java
+++ core/src/main/java/net/adoptopenjdk/icedteaweb/proxy/ie/WindowsProxy.java
@@ -1,33 +1,69 @@
 package net.adoptopenjdk.icedteaweb.proxy.ie;
 
-import static net.adoptopenjdk.icedteaweb.proxy.ie.WinRegistryConstants.PROXY_ENABLED_VAL;
-import static net.adoptopenjdk.icedteaweb.proxy.ie.WinRegistryConstants.PROXY_REGISTRY_KEY;
-import static net.adoptopenjdk.icedteaweb.proxy.ie.WinRegistryConstants.PROXY_SERVER_OVERRIDE_VAL;
-import static net.adoptopenjdk.icedteaweb.proxy.ie.WinRegistryConstants.PROXY_SERVER_REGISTRY_VAL;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.concurrent.ExecutionException;
+import java.util.regex.Pattern;
+
+import static net.adoptopenjdk.icedteaweb.proxy.ie.WindowsProxyConstants.AUTO_CONFIG_URL_VAL;
+import static net.adoptopenjdk.icedteaweb.proxy.ie.WindowsProxyConstants.PROXY_ENABLED_VAL;
+import static net.adoptopenjdk.icedteaweb.proxy.ie.WindowsProxyConstants.PROXY_REGISTRY_KEY;
+import static net.adoptopenjdk.icedteaweb.proxy.ie.WindowsProxyConstants.PROXY_SERVER_OVERRIDE_VAL;
+import static net.adoptopenjdk.icedteaweb.proxy.ie.WindowsProxyConstants.PROXY_SERVER_REGISTRY_VAL;
 
 public class WindowsProxy {
 
+    public static void printProxySettings() throws InterruptedException, ExecutionException, IOException {
+        final RegistryValue proxyEnabledValue = RegistryQuery.getRegistryValue(PROXY_REGISTRY_KEY, PROXY_ENABLED_VAL).orElse(null);
+        if(proxyEnabledValue != null && proxyEnabledValue.getValueAsBoolean() ) {
+            System.out.println("Windows Proxy Server is enabled");
+
+            final RegistryValue autoConfigUrlValue = RegistryQuery.getRegistryValue(PROXY_REGISTRY_KEY, AUTO_CONFIG_URL_VAL).orElse(null);
+            if(autoConfigUrlValue != null) {
+                System.out.println("Windows Proxy Server use PAC with URL: " + autoConfigUrlValue.getValue());
+            } else {
+                final RegistryValue proxyServerValue = RegistryQuery.getRegistryValue(PROXY_REGISTRY_KEY, PROXY_SERVER_REGISTRY_VAL).orElse(null);
+                if(proxyServerValue != null) {
+                    Arrays.asList(proxyServerValue.getValue().split(Pattern.quote(";"))).forEach(p -> System.out.println("Proxy: " + p));
+                } else {
+                    System.out.println("No specific Proxy server defined. DIRECT will be used");
+                }
+                final RegistryValue overrideHostsValue = RegistryQuery.getRegistryValue(PROXY_REGISTRY_KEY, PROXY_SERVER_OVERRIDE_VAL).orElse(null);
+                if(overrideHostsValue != null) {
+                    Arrays.asList(overrideHostsValue.getValue().split(Pattern.quote(";"))).forEach(p -> System.out.println("Exclusion: " + p));
+                } else {
+                    System.out.println("No exlusion defined");
+                }
+            }
+        } else {
+            System.out.println("Windows Proxy Server is not enabled");
+        }
+    }
+
     public static void main(String[] args) throws Exception {
 
-        final RegistryValue proxyServerValue = RegistryQuery.getRegistryValue(PROXY_REGISTRY_KEY, PROXY_SERVER_REGISTRY_VAL).orElse(null);
-        if(proxyServerValue != null) {
-            System.out.println("Windows Proxy Server setting: '" + proxyServerValue.getValue() + "'");
-        } else {
-            System.out.println("Windows Proxy Server setting not defined");
-        }
+        printProxySettings();
 
-        final RegistryValue overrideHostsValue = RegistryQuery.getRegistryValue(PROXY_REGISTRY_KEY, PROXY_SERVER_OVERRIDE_VAL).orElse(null);
-        if(overrideHostsValue != null) {
-            System.out.println("Windows Proxy Server override hosts setting: '" + overrideHostsValue.getValue() + "'");
-        } else {
-            System.out.println("Windows Proxy Server override hosts setting not defined");
-        }
 
-        final RegistryValue proxyEnabledValue = RegistryQuery.getRegistryValue(PROXY_REGISTRY_KEY, PROXY_ENABLED_VAL).orElse(null);
-        if(proxyEnabledValue != null) {
-            System.out.println("Windows Proxy Server enabled: '" + proxyEnabledValue.getValueAsBoolean() + "'");
-        } else {
-            System.out.println("Windows Proxy Server enabled not defined");
-        }
+//        final RegistryValue proxyServerValue = RegistryQuery.getRegistryValue(PROXY_REGISTRY_KEY, PROXY_SERVER_REGISTRY_VAL).orElse(null);
+//        if(proxyServerValue != null) {
+//            System.out.println("Windows Proxy Server setting: '" + proxyServerValue.getValue() + "'");
+//        } else {
+//            System.out.println("Windows Proxy Server setting not defined");
+//        }
+//
+//        final RegistryValue overrideHostsValue = RegistryQuery.getRegistryValue(PROXY_REGISTRY_KEY, PROXY_SERVER_OVERRIDE_VAL).orElse(null);
+//        if(overrideHostsValue != null) {
+//            System.out.println("Windows Proxy Server override hosts setting: '" + overrideHostsValue.getValue() + "'");
+//        } else {
+//            System.out.println("Windows Proxy Server override hosts setting not defined");
+//        }
+//
+//        final RegistryValue proxyEnabledValue = RegistryQuery.getRegistryValue(PROXY_REGISTRY_KEY, PROXY_ENABLED_VAL).orElse(null);
+//        if(proxyEnabledValue != null) {
+//            System.out.println("Windows Proxy Server enabled: '" + proxyEnabledValue.getValueAsBoolean() + "'");
+//        } else {
+//            System.out.println("Windows Proxy Server enabled not defined");
+//        }
     }
 }
