720ad604150f8604a0a70bbd4f59391a20a101fb
AndreasEhret <andreas.ehret@karakun.com>
2020-02-11 11:28:56 +0100

    new base dialogs

---
+++ common/src/main/java/net/adoptopenjdk/icedteaweb/ui/dialogs/ButtonBasedDialogWithResult.java
@@ -0,0 +1,22 @@
+package net.adoptopenjdk.icedteaweb.ui.dialogs;
+
+import javax.swing.JPanel;
+import java.util.Arrays;
+import java.util.List;
+
+public abstract class ButtonBasedDialogWithResult<R> extends DialogWithResult<R> {
+
+    private final List<DialogButton<R>> buttons;
+
+    public ButtonBasedDialogWithResult(final String title, final DialogButton<R>... buttons) {
+        super(title);
+        this.buttons = Arrays.asList(buttons);
+    }
+
+    protected JPanel createContentPane() {
+        return createContentPane(buttons);
+    }
+
+    protected abstract JPanel createContentPane(final List<DialogButton<R>> buttons);
+
+}
---
+++ common/src/main/java/net/adoptopenjdk/icedteaweb/ui/dialogs/DialogButton.java
@@ -0,0 +1,36 @@
+package net.adoptopenjdk.icedteaweb.ui.dialogs;
+
+import net.adoptopenjdk.icedteaweb.Assert;
+
+import java.util.function.Supplier;
+
+public class DialogButton<R> {
+
+    private final String text;
+
+    private final Supplier<R> onAction;
+
+    private final String description;
+
+    public DialogButton(final String text, final Supplier<R> onAction) {
+        this(text, onAction, null);
+    }
+
+    public DialogButton(final String text, final Supplier<R> onAction, final String description) {
+        this.text = Assert.requireNonBlank(text, "text");
+        this.onAction = Assert.requireNonNull(onAction, "onAction");
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public Supplier<R> getOnAction() {
+        return onAction;
+    }
+}
