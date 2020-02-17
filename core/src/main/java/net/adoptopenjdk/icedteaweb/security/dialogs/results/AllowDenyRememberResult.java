6fe6fb0fbb0d0fdc78fd1aa8da7136328a1b7e82
AndreasEhret <andreas.ehret@karakun.com>
2020-02-17 08:55:40 +0100

    make result public

--- core/src/main/java/net/adoptopenjdk/icedteaweb/security/dialogs/results/AllowDenyRememberResult.java
+++ core/src/main/java/net/adoptopenjdk/icedteaweb/security/dialogs/results/AllowDenyRememberResult.java
@@ -6,7 +6,7 @@ public class AllowDenyRememberResult {
     private final AllowDeny allowDenyResult;
     private final RememberResult rememberResult;
 
-    AllowDenyRememberResult(final AllowDeny allowDenyResult, final RememberResult rememberResult) {
+    public AllowDenyRememberResult(final AllowDeny allowDenyResult, final RememberResult rememberResult) {
         this.allowDenyResult = allowDenyResult;
         this.rememberResult = rememberResult;
     }
