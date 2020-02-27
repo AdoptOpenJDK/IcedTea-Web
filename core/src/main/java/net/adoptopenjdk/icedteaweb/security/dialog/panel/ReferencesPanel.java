d3374b85ed0ad4f508bf8fef775451d3feaebdc6
AndreasEhret <andreas.ehret@karakun.com>
2020-02-27 13:59:05 +0100

    remove unused method

--- core/src/main/java/net/adoptopenjdk/icedteaweb/security/dialog/panel/ReferencesPanel.java
+++ core/src/main/java/net/adoptopenjdk/icedteaweb/security/dialog/panel/ReferencesPanel.java
@@ -2,7 +2,6 @@ package net.adoptopenjdk.icedteaweb.security.dialog.panel;
 
 import net.adoptopenjdk.icedteaweb.StringUtils;
 import net.adoptopenjdk.icedteaweb.client.util.html.HtmlUtil;
-import net.adoptopenjdk.icedteaweb.client.util.html.NamedUrl;
 import net.adoptopenjdk.icedteaweb.i18n.Translator;
 import net.adoptopenjdk.icedteaweb.logging.Logger;
 import net.adoptopenjdk.icedteaweb.logging.LoggerFactory;
@@ -17,7 +16,6 @@ import java.awt.Font;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.net.URL;
-import java.util.List;
 import java.util.Set;
 
 public class ReferencesPanel extends JPanel {
@@ -29,20 +27,15 @@ public class ReferencesPanel extends JPanel {
         createContent(null, htmlList);
     }
 
-    public ReferencesPanel(String title, Set<URL> urls) {
+    public ReferencesPanel(final String title, final Set<URL> urls) {
         super(new BorderLayout());
         createContent(title, HtmlUtil.unorderedListOf(urls, 4));
-    }
-
-    public ReferencesPanel(String title, List<NamedUrl> namedUrls) {
-        super(new BorderLayout());
-        createContent(title, HtmlUtil.unorderedListOf(namedUrls, 4));
     }
 
     private void createContent(final String listTitle, final String htmlList) {
         final String html = StringUtils.isBlank(listTitle) ? htmlList : listTitle + "<br/>" + htmlList;
 
-        JEditorPane editorPane = new JEditorPane("text/html", html);
+        final JEditorPane editorPane = new JEditorPane("text/html", html);
         editorPane.putClientProperty(JEditorPane.HONOR_DISPLAY_PROPERTIES, Boolean.TRUE);
         editorPane.setEditable(false);
         editorPane.setFont(new Font(Font.SANS_SERIF, getFont().getStyle(), getFont().getSize()));
