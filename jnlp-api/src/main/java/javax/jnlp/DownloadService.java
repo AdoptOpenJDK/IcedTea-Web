d0ad7fb3fbde68a250b647acaa1d5e404a98ce3f
Stephan Classen <st.classen@gmx.ch>
2020-03-20 17:08:25 +0100

    java doc

--- jnlp-api/src/main/java/javax/jnlp/DownloadService.java
+++ jnlp-api/src/main/java/javax/jnlp/DownloadService.java
@@ -53,24 +53,112 @@ public interface DownloadService {
      */
     boolean isExtensionPartCached(URL ref, String version, String[] parts);
 
+    /**
+     * Downloads the given resource, if the resource is either mentioned in the calling applications JNLP file,
+     * is within the codebase of the calling applications JNLP file, or if the calling application has been
+     * granted all-permissions. This method will block until the download is completed or an exception occurs.
+     *
+     * @param ref The URL for the resource.
+     * @param version The version string, or null for no version.
+     * @param progress Download progress callback object.
+     * @throws java.io.IOException if an I/O error occurs
+     */
     void loadResource(URL ref, String version, DownloadServiceListener progress) throws java.io.IOException;
 
+    /**
+     * Downloads the given part, if the part is mentioned in the JNLP file for the application.
+     * This method will block until the download is completed or an exception occurs.
+     *
+     * @param part The name of the part.
+     * @param progress Download progress callback object.
+     * @throws java.io.IOException if an I/O error occurs
+     */
     void loadPart(String part, DownloadServiceListener progress) throws java.io.IOException;
 
+    /**
+     * Downloads the given parts, if the parts are mentioned in the JNLP file for the application.
+     * This method will block until the download is completed or an exception occurs.
+     *
+     * @param parts An array of part names.
+     * @param progress Download progress callback object.
+     * @throws java.io.IOException if an I/O error occurs
+     */
     void loadPart(String[] parts, DownloadServiceListener progress) throws java.io.IOException;
 
+    /**
+     * Downloads the given part of the given extension,
+     * if the part and the extension are mentioned in the JNLP file for the application.
+     * This method will block until the download is completed or an exception occurs.
+     *
+     * @param ref The URL for the resource.
+     * @param version The version string, or null for no version.
+     * @param part The name of the part.
+     * @param progress Download progress callback object.
+     * @throws java.io.IOException if an I/O error occurs
+     */
     void loadExtensionPart(URL ref, String version, String part, DownloadServiceListener progress) throws java.io.IOException;
 
+    /**
+     * Downloads the given parts of the given extension,
+     * if the parts and the extension are mentioned in the JNLP file for the application.
+     * This method will block until the download is completed or an exception occurs.
+     *
+     * @param ref The URL for the resource.
+     * @param version The version string, or null for no version.
+     * @param parts An array of part names to load.
+     * @param progress Download progress callback object.
+     * @throws java.io.IOException if an I/O error occurs
+     */
     void loadExtensionPart(URL ref, String version, String[] parts, DownloadServiceListener progress) throws java.io.IOException;
 
+    /**
+     * Removes the given resource from the cache,
+     * if the resource is either mentioned in the calling applications JNLP file,
+     * is within the codebase of the calling applications JNLP file,
+     * or if the calling application has been granted all-permissions.
+     *
+     * @param ref The URL for the resource.
+     * @param version The version string, or null for no version.
+     * @throws java.io.IOException if an I/O error occurs
+     */
     void removeResource(URL ref, String version) throws java.io.IOException;
 
+    /**
+     * Removes the given part from the cache, if the part is mentioned in the JNLP file for the application.
+     *
+     * @param part The name of the part.
+     * @throws java.io.IOException if an I/O error occurs
+     */
     void removePart(String part) throws java.io.IOException;
 
+    /**
+     * Removes the given parts from the cache, if the parts are mentioned in the JNLP file for the application.
+     *
+     * @param parts An array of part names.
+     * @throws java.io.IOException if an I/O error occurs
+     */
     void removePart(String[] parts) throws java.io.IOException;
 
+    /**
+     * Removes the given part of the given extension from the cache,
+     * if the part and the extension are mentioned in the JNLP file for the application.
+     *
+     * @param ref The URL for the resource.
+     * @param version The version string, or null for no version.
+     * @param part The name of the part.
+     * @throws java.io.IOException if an I/O error occurs
+     */
     void removeExtensionPart(URL ref, String version, String part) throws java.io.IOException;
 
+    /**
+     * Removes the given parts of the given extension from the cache,
+     * if the parts and the extension are mentioned in the JNLP file for the application.
+     *
+     * @param ref The URL for the resource.
+     * @param version The version string, or null for no version.
+     * @param parts An array of part names.
+     * @throws java.io.IOException if an I/O error occurs
+     */
     void removeExtensionPart(URL ref, String version, String[] parts) throws java.io.IOException;
 
     /**
