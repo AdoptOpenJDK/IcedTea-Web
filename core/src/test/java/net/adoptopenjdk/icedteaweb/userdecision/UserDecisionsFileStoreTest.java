d572a183c7cf4dfd3ff31899327b8595071a605a
Stephan Classen <st.classen@gmx.ch>
2020-04-08 15:24:01 +0200

    cleanup test

--- core/src/test/java/net/adoptopenjdk/icedteaweb/userdecision/UserDecisionsFileStoreTest.java
+++ core/src/test/java/net/adoptopenjdk/icedteaweb/userdecision/UserDecisionsFileStoreTest.java
@@ -1,13 +1,15 @@
 package net.adoptopenjdk.icedteaweb.userdecision;
 
 import net.adoptopenjdk.icedteaweb.security.dialog.result.AllowDeny;
+import net.adoptopenjdk.icedteaweb.xmlparser.ParseException;
 import net.sourceforge.jnlp.JNLPFile;
 import net.sourceforge.jnlp.JNLPFileFactory;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 
-import java.io.File;
+import java.io.IOException;
 import java.util.Optional;
 
 import static net.adoptopenjdk.icedteaweb.security.dialog.result.AllowDeny.DENY;
@@ -23,19 +25,27 @@ public class UserDecisionsFileStoreTest {
     @Rule
     public TemporaryFolder temporaryFolder = new TemporaryFolder();
 
+    private UserDecisions userDecisions;
+
+    @Before
+    public final void setUp() throws Exception {
+        userDecisions = new UserDecisionsFileStore(temporaryFolder.newFile("userDecisionStore.json"));
+    }
+
     @Test
     public void shouldSaveDecisionInFileStore() throws Exception {
         // given
-        final JNLPFile file = new JNLPFileFactory().create(getClass().getResource("/net/sourceforge/jnlp/basic.jnlp"));
-        final File store = temporaryFolder.newFile("userDecisionStore.json");
-        final UserDecisions userDecisions = new UserDecisionsFileStore(store);
+        final JNLPFile file = loadJnlpFile("/net/sourceforge/jnlp/basic.jnlp");
 
         // when
         userDecisions.saveForApplication(file, UserDecision.of(RUN_UNSIGNED_APPLICATION, DENY));
         final Optional<AllowDeny> result = userDecisions.getUserDecisions(RUN_UNSIGNED_APPLICATION, file, AllowDeny.class);
 
         // then
-        assertThat(result.isPresent(), is(true));
-        assertThat(result.get(), is(DENY));
+        assertThat(result, is(Optional.of(DENY)));
+    }
+
+    private JNLPFile loadJnlpFile(String name) throws IOException, ParseException {
+        return new JNLPFileFactory().create(getClass().getResource(name));
     }
 }
