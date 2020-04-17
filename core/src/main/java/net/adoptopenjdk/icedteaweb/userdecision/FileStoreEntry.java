4ced749212c6ec2e9d7cb4234a071110c739b4ec
Stephan Classen <st.classen@gmx.ch>
2020-04-17 23:40:45 +0200

    change file store impl to make json more compact

--- core/src/main/java/net/adoptopenjdk/icedteaweb/userdecision/FileStoreEntry.java
+++ core/src/main/java/net/adoptopenjdk/icedteaweb/userdecision/FileStoreEntry.java
@@ -1,54 +1,34 @@
 package net.adoptopenjdk.icedteaweb.userdecision;
 
-import net.adoptopenjdk.icedteaweb.Assert;
-
 import java.net.URL;
-import java.util.Objects;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
 
-public class FileStoreEntry {
-    private UserDecision.Key userDecisionKey;
-    private String userDecisionValue;
+class FileStoreEntry {
+    private final URL domain;
+    private final Set<String> jarNames;
+    private final Map<UserDecision.Key, String> decisions = new HashMap<>();
 
-    private URL domain;
-    private Set<String> jarNames;
-
-    public <T extends Enum<T>> FileStoreEntry(UserDecision.Key userDecisionKey, String userDecisionValue, final URL domain, final Set<String> jarNames) {
-        this.userDecisionKey = Assert.requireNonNull(userDecisionKey, "userDecisionKey");
-        this.userDecisionValue = Assert.requireNonNull(userDecisionValue, "userDecisionValue");
-        this.domain = Assert.requireNonNull(domain, "domain");
-        this.jarNames = Assert.requireNonNull(jarNames, "jarNames");
+    FileStoreEntry(final URL domain, final Set<String> jarNames) {
+        this.domain = domain;
+        this.jarNames = jarNames;
     }
 
-    public UserDecision.Key getUserDecisionKey() {
-        return userDecisionKey;
+    Optional<String> getUserDecisionValue(UserDecision.Key key) {
+        return Optional.ofNullable(decisions.get(key));
     }
 
-    public String getUserDecisionValue() {
-        return userDecisionValue;
+    <T extends Enum<T>> void setUserDecision(UserDecision<T> userDecision) {
+        decisions.put(userDecision.getKey(), userDecision.getValue().name());
     }
 
-    public URL getDomain() {
+    URL getDomain() {
         return domain;
     }
 
-    public Set<String> getJarNames() {
+    Set<String> getJarNames() {
         return jarNames;
-    }
-
-    @Override
-    public boolean equals(final Object o) {
-        if (this == o) return true;
-        if (!(o instanceof FileStoreEntry)) return false;
-        final FileStoreEntry that = (FileStoreEntry) o;
-        return userDecisionKey.equals(that.userDecisionKey) &&
-                userDecisionValue.equals(that.userDecisionValue) &&
-                domain.equals(that.domain) &&
-                jarNames.equals(that.jarNames);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(userDecisionKey, userDecisionValue, domain, jarNames);
     }
 }
