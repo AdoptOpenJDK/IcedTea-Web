a7ee1f3efe653e87c6e20d00a2cf1e81eddbdf44
Hendrik Ebbers <hendrik.ebbers@web.de>
2020-05-22 14:51:40 +0200

    Licence header formatting

--- core/src/main/java/net/sourceforge/jnlp/services/package-info.java
+++ core/src/main/java/net/sourceforge/jnlp/services/package-info.java
@@ -29,7 +29,8 @@ conditions of the license of that module. An independent module is a module
 which is not derived from or based on this library. If you modify this library,
 you may extend this exception to your version of the library, but you are not
 obligated to do so. If you do not wish to do so, delete this exception
-statement from your version.*/
+statement from your version.
+*/
 /**
  * This package contains the classes that implement the standard services 
  * defined by the JNLP specification.
