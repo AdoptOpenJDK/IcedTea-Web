5330dec000f1f4f8ba515c0b0ea011f273605e3f
Roettges Florian <roettges.florian@scheidt-bachmann.de>
2020-07-31 11:43:21 +0200

    Added finally block to ensure classloader is restored in case of exceptions

--- core/src/main/java/net/sourceforge/jnlp/runtime/AppContextFactory.java
+++ core/src/main/java/net/sourceforge/jnlp/runtime/AppContextFactory.java
@@ -8,13 +8,15 @@ public class AppContextFactory {
         //set temporary classloader for EventQueue initialization
         //already a call to AppContext.getAppContext(...) initializes the EventQueue.class
         ClassLoader originalLoader = Thread.currentThread().getContextClassLoader();
-        DelegatingClassLoader delegatingLoader = DelegatingClassLoader.getInstance();
-        delegatingLoader.setClassLoader(originalLoader);
+        try {
+            DelegatingClassLoader delegatingLoader = DelegatingClassLoader.getInstance();
+            delegatingLoader.setClassLoader(originalLoader);
 
-        Thread.currentThread().setContextClassLoader(delegatingLoader);
-
-        SunToolkit.createNewAppContext();
-        //restore original classloader
-        Thread.currentThread().setContextClassLoader(originalLoader);
+            Thread.currentThread().setContextClassLoader(delegatingLoader);
+            SunToolkit.createNewAppContext();
+        }finally {
+            //restore original classloader
+            Thread.currentThread().setContextClassLoader(originalLoader);
+        }
     }
 }
